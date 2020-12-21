58b3e22b8179286b848778c588c0ca084f33895d
Joshua Parco <jacparco@gmail.com>
2020-12-21 09:29:20 -0700

    initial commit

---
+++ src/components/Account.js
@@ -0,0 +1,10 @@
+import { Card, } from "semantic-ui-react";
+
+const Account = () => (
+  <Card>
+    <Card.Content>
+      <Card.Header>username</Card.Header>
+  </Card>
+)
+
+export default Account;
